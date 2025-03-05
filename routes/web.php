4bfd3412e65f3c968fe610ef869e3eca334a7b45
Ayhan <afraitteam@gmail.com>
2025-03-05 16:50:57 +0330

    First commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('products/all', function () {
+    return view('frontend.products.all');
+});
+
+
+Route::get('/admin/index', function () {
+    return view('admin.index');
+});
+
+
+Route::get('/admin/users/', function () {
+    return view('admin.users.index');
+
+});
